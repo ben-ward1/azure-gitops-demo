873caf5eeaedf289ab99ad255e96adf2611c229f
Ben Ward <42178651+ben-ward1@users.noreply.github.com>
2023-04-10 12:55:09 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,11 @@
 terraform {
   required_version = ">= 0.15"
+  backend "azurerm" {
+    storage_account_name = "__storageAccountName__"
+    container_name       = "__containerName__"
+    key                  = "terraform.tfstate"
+    access_key           = "__storageKey__"
+  }
 }
 
 provider "azurerm" {
